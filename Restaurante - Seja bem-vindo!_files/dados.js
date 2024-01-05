799e5744d61a1a920268b375bf724b6c83d3bcd8
Otacílio <otacilio.junior.quirino.713@ufrn.edu.br>
2024-01-05 11:05:33 -0300

    meu primeiro commit

---
+++ Restaurante - Seja bem-vindo!_files/dados.js
@@ -0,0 +1,94 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
+            "name": "Seleção de Pastéis",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 0
+        },
+    ],
+    "burgers": [
+        
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
+            "name": "sobremesas",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 0
+        },
+        
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "name": "Variedade de Bebidas",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 0
+        },
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/2-lou-malnatis-deep-dish-pizzas.bf0fe065d251a9cca3925b269d443a27.jpg",
+            "name": "Pizza Margherita", // alterado
+            "dsc": "2 Lou Malnati's Deep DisDFFGDFGFD",
+            "price": 35
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
+            "name": "Frango Catupiry",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 35
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-new-haven-style-pizza-6-pack.ab82828afc6172cdd4017556c15e36dd.jpg",
+            "name": "Frango Cheddar",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 35
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/6-lou-malnatis-deep-dish-pizzas.f59993181da5d295668c8a6fb856055e.jpg",
+            "name": "Calabresa e Alho",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 35
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/pizzas/wood-fired-pizzas-best-seller-4-pack.1653bb05922ba153ac178f8365d27f6d.jpg",
+            "name": "Milho",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 35
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-deep-dish-pizza-3-pack.4111791511244a4946bb5c9ad2c17da9.jpg",
+            "name": "Calabresa",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 35
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/detroit-style-pizza-choose-your-own-3-pack.6b6f4909ffd4066d5471e70eac5c3d89.jpg",
+            "name": "Portuguesa",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 35
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/pizzas/brooklyn-pizza-choose-your-own-5-pack.edc4f476a75207d0af840ce6f225f2b3.jpg",
+            "name": "Carne Catupiry",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 40
+        },
+        
+    ],
+    "steaks": [
+    ]
+}
